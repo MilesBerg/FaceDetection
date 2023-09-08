36f75dc20efe438ed5f46ce6d04fe29a2abadfbd
Miles Berg <116774321+MilesBerg@users.noreply.github.com>
2023-09-08 14:35:34 -0500

    Update face.py

--- face.py
+++ face.py
@@ -2,19 +2,15 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-# Select image for face detection Ex. miley.jpg
 image_path = 'miley.jpg'
 image = cv2.imread(image_path)
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Perform face detection
 faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-# Draw rectangles around the detected faces
 for (x, y, w, h) in faces:
     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-# Display the result
 cv2.imshow('Face Detection', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
